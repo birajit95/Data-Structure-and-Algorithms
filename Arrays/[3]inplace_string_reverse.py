37f604ba290519a61d81c64b252d21d13666ac1d
Birajit <birajit95@gmail.com>
2026-02-02 23:11:54 +0530

    add: inplace string reverse solved

---
+++ Arrays/[3]inplace_string_reverse.py
@@ -0,0 +1,48 @@
+"""
+344. Reverse String
+
+
+Write a function that reverses a string. The input string is given as an array of characters s.
+
+You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"]
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 105
+    s[i] is a printable ascii character.
+
+
+"""
+
+
+from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        i = 0
+        j = n-1
+
+        while i<j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1
+        
